491e9eb866d0e0534b597b6efc4add95da31059f
mehrabix <mehrabi@post.com>
2021-12-13 01:02:56 +0330

    add cookie data

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,13 @@
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import Cookies from "js-cookie";
+import { useRouter } from "next/router";
+
 
 export default function Home() {
+  const router = useRouter();
+
+
   const userData = [
     {
       id: 1,
@@ -37,18 +42,19 @@ export default function Home() {
 
   }, []);
 
-  
   const [userDataApi, setUserDataApi] = useState([]);
   const [emailborder, setEmailBorder] = useState("border-black");
   const [passwordborder, setPasswordBorder] = useState("border-black");
   const [emailErrorMessage, setEmailMessageText] = useState("");
   const [passwordErrorMessage, setPasswordMessageText] = useState("");
+  const [messageBoxText, setMessageBoxText] = useState("");
+  const [messageBoxColor, setMessageBoxColor] = useState("");
+  const [messageBoxDisplay, setMessageBoxDisplay] = useState("h-0 overflow-hidden");
 
   const handlePasswordInputValidation = (e) => {
     e.preventDefault();
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    // check if inputs are empty
     if (email === "") {
       setEmailBorder("border-red-600");
       setEmailMessageText("ایمیل خود را وارد کنید");
@@ -64,12 +70,19 @@ export default function Home() {
       );
       if (user) {
         Cookies.set("user", JSON.stringify(user));
-        window.location.href = "/dashboard";
+        setMessageBoxDisplay("h-16");
+        setMessageBoxColor("border-[#299603] bg-[#33AE08]");
+        setMessageBoxText("با موفقیت وارد داشبورد شدید");
+        router.push("/dashboard");
+        
       } else {
         setEmailBorder("border-red-600");
         setPasswordBorder("border-red-600");
-        setEmailMessageText("ایمیل  اشتباه است");
-        setPasswordMessageText(" رمز عبور اشتباه است");
+        setEmailMessageText("خطای ورود");
+        setMessageBoxDisplay("h-16");
+        setMessageBoxColor("border-[#AD2929] bg-[#CB444A]");
+        setPasswordMessageText(" خطای ورود");
+        setMessageBoxText("ایمیل یا رمز عبور اشتباه است");
       }
       
     }
@@ -77,19 +90,8 @@ export default function Home() {
 
   return (
     <>
-      {/* {
-        userDataApi.map((user) => {
-          return (
-            <div key={user.id}>
-              <h1>{user.email}</h1>
-              <h1>{user.password}</h1>
-            </div>
-          );
-        }
-        )
-    }   */}
-      <div dir="rtl" className="h-screen flex justify-center items-center">
-        <div className="bg-white w-[90vw] h-[50vh] md:w-[70vw] md:h-[50vh] lg:w-[32vw] lg:h-[59vh] mx-auto my-auto rounded-lg shadow-lg">
+      <div dir="rtl" className="h-screen flex justify-center items-center relative">
+        <div className="bg-white w-[90vw] h-[60vh] md:w-[70vw] md:h-[50vh] lg:w-[32vw] lg:h-[59vh] mx-auto my-auto rounded-lg shadow-lg">
           <form
             onSubmit={(e) => handlePasswordInputValidation(e)}
             className="flex flex-col justify-center mx-[10%] h-full  font-Shabnam space-y-4"
@@ -126,9 +128,9 @@ export default function Home() {
                 ورود به حساب کاربری
               </button>
               <div className="flex items-center mt-3">
-                <p className="text-sm">رمز عبور خود را فراموش کرده اید؟</p>
+                <p className="text-xs md:text-sm">رمز عبور خود را فراموش کرده اید؟</p>
 
-                <div className="text-[#007BFF] font-ShabnamBold text-sm mr-1">
+                <div className="text-[#007BFF] font-ShabnamBold text-xs md:text-sm mr-1">
                   <Link className="" href="/forget">
                     بازیابی رمز عبور
                   </Link>
@@ -136,6 +138,9 @@ export default function Home() {
               </div>
             </div>
           </form>
+        </div>
+        <div className={`w-[234px] border-l-8 ${messageBoxColor} absolute transition-all ease-linear duration-300 left-6 bottom-6 ${messageBoxDisplay} font-Shabnam text-white flex justify-center items-center `}>
+          {messageBoxText}
         </div>
       </div>
     </>
