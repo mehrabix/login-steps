c1f07fc11165fe9a921f300153209c77ca4685d2
mehrabix <mehrabi@post.com>
2021-12-13 00:20:47 +0330

    add cookie data

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        iranSans: "iranSans",
+        iranSansBlack: "iranSansBlack",
+        iranSansBold: "iranSansBold",
+        iranSansLight: "iranSansLight",
+        iranSansMedium: "iranSansMedium",
+        iranSansUltraLight: "iranSansUltraLight",
+        iranSansMobile: "iranSansMobile",
+        iranSansMobileBlack: "iranSansMobileBlack",
+        iranSansMobileBold: "iranSansMobileBold",
+        iranSansMobileLight: "iranSansMobileLight",
+        iranSansMobileMedium: "iranSansMobileMedium",
+        iranSansMobileUltraLight: "iranSansMobileUltraLight",
+        Shabnam: "Shabnam",
+        ShabnamLight: "Shabnam-Light",
+        ShabnamBold: "Shabnam-Bold",
+      },
+    },
+  },
+  plugins: [],
+};
